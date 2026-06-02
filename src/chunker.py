48ef6b329533e8c455d8f780c6fb60a87de8e17d
Rakeshpm99 <142134851+Rakeshpm99@users.noreply.github.com>
2026-06-02 19:58:11 +0530

    Delete src/chunker.py

--- src/chunker.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from langchain_core.documents import Document
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-def split_documents(documents: List[Document]) -> List[Document]:
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
-    return text_splitter.split_documents(documents)
-    
