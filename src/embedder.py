601186dd1554e2576208fd2a064d6d424eeac52c
Rakeshpm99 <rakeshrakupm123@gmail.com>
2026-06-03 19:27:21 +0000

    fix:  embedding logic

--- src/embedder.py
+++ src/embedder.py
@@ -1,19 +1,7 @@
-import chromadb
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-import os
 
-def build_vector_store(chunks, persist_directory="./chroma_db"):
+def build_vector_store(chunks):
+    # Swapped from Nomic to BAAI for faster local latency
     embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
-
-    if not os.path.exists(persist_directory):
-        os.makedirs(persist_directory)
-
-    client = chromadb.PersistentClient(path=persist_directory)
-
-    return Chroma.from_documents(
-        documents=chunks,
-        embedding=embeddings,
-        client=client,
-        collection_name="rag_collection"
-    )
+    return Chroma.from_documents(chunks, embeddings, persist_directory="/chroma_db_test")
