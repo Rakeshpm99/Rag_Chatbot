0820e375ac9ad479307d5f00a57fe6fae1f212ba
Rakeshpm99 <rakeshrakupm123@gmail.com>
2026-06-04 05:36:45 +0000

    Update

--- src/embedder.py
+++ src/embedder.py
@@ -1,7 +1,11 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 
+
 def build_vector_store(chunks):
-    # Swapped from Nomic to BAAI for faster local latency
     embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
-    return Chroma.from_documents(chunks, embeddings, persist_directory="/chroma_db_test")
+    return Chroma.from_documents(
+        chunks,
+        embeddings,
+        persist_directory="./chroma_db_test"
+    )
